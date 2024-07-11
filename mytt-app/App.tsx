ae40b6da79ddc1be86e871597021a3dc7389102e
Flayre <mariesamantha.f@gmail.com>
2024-07-11 11:24:41 -0700

    Stash

--- mytt-app/App.tsx
+++ mytt-app/App.tsx
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { View } from "react-native";
+import React from "react";
+import SplashScreen from "./components/SplashScreen";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={{ flex: 1, backgroundColor: "#2166DE" }}>
+			<SplashScreen />
+			<StatusBar style="auto" />
+		</View>
+	);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ mytt-app/components/SplashScreen.js
@@ -0,0 +1,105 @@
+import React, { useState, useEffect } from "react";
+import { View, Image, StyleSheet, Animated, Easing, Text } from "react-native";
+import * as Font from "expo-font";
+
+const SplashScreen = () => {
+	const [fadeAnim] = useState(new Animated.Value(0));
+	const [positionAnim] = useState(new Animated.Value(0));
+	const [buttonFadeAnim] = useState(new Animated.Value(0));
+	const [isFontLoaded, setIsFontLoaded] = useState(false);
+
+	useEffect(() => {
+		const loadFont = async () => {
+			await Font.loadAsync({
+				Poppins: require("../assets/Poppins-Regular.ttf"),
+			});
+			setIsFontLoaded(true);
+		};
+		loadFont();
+
+		const positionAnimation = Animated.timing(positionAnim, {
+			toValue: -50,
+			duration: 500,
+			useNativeDriver: true,
+			easing: Easing.linear,
+		});
+
+		const fadeUpAnimation = Animated.timing(fadeAnim, {
+			toValue: 1,
+			duration: 1500,
+			useNativeDriver: true,
+			easing: Easing.linear,
+		});
+
+		Animated.sequence([fadeUpAnimation, positionAnimation]).start(() => {
+			Animated.timing(buttonFadeAnim, {
+				toValue: 1,
+				duration: 1000,
+				useNativeDriver: true,
+				easing: Easing.linear,
+			}).start();
+		});
+	}, []);
+
+	if (!isFontLoaded) {
+		return null;
+	}
+
+	return (
+		<View style={styles.container}>
+			<Animated.View
+				style={[
+					styles.fadeContainer,
+					{
+						opacity: fadeAnim,
+						transform: [{ translateY: positionAnim }],
+					},
+				]}
+			>
+				<Image
+					source={require("../assets/MYTT_Logo.png")}
+					style={{ width: 154, height: 93 }}
+					resizeMode="contain"
+				/>
+			</Animated.View>
+			{/* <Text style={styles.text}>Welcome to the MYTT App</Text> */}
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	backgroundImage: {
+		flex: 1,
+		width: "100%",
+		height: "100%",
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	fadeContainer: {
+		alignItems: "center",
+	},
+	image: {
+		width: 280,
+		height: 280,
+	},
+	text: {
+		fontSize: 20,
+		color: "#F7F7E1",
+		fontFamily: "Poppins",
+		fontWeight: "bold",
+	},
+	button: {
+		marginTop: 20,
+		padding: 10,
+		backgroundColor: "#4CAF50",
+		color: "white",
+		fontSize: 20,
+	},
+});
+
+export default SplashScreen;
