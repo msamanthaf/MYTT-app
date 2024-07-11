e8045b4f5a58ed5569ff36598cfba8947b952198
Mohamed Al-Nassirat <malnassi@ualberta.ca>
2024-07-11 10:39:19 -0700

    NavBar
    Added the NavBar with Calender Home And Profile Screen

--- mytt-app/App.tsx
+++ mytt-app/App.tsx
@@ -1,12 +1,25 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from './navbar/HomeScreen';
+import CalendarScreen from './navbar/CalendarScreen'; 
+import ProfileScreen from './navbar/ProfileScreen'; 
+
+// Define the tab navigator
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Calendar" component={CalendarScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+      </Tab.Navigator>
       <StatusBar style="auto" />
-    </View>
+    </NavigationContainer>
   );
 }
 
