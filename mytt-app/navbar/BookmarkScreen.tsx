8bc67645278d20ab9ae458ac487adc30a64528bd
Flayre <mariesamantha.f@gmail.com>
2024-07-11 19:49:44 -0700

    Bookmarked page done

--- mytt-app/navbar/BookmarkScreen.tsx
+++ mytt-app/navbar/BookmarkScreen.tsx
@@ -1,10 +1,44 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { Program } from '../models'; // Make sure to import your Program type from the correct location
+import ProgramCard from '../components/ProgramCard'; // Assuming ProgramCard is a component that displays program details
+
+const programs: Program[] = [
+    {
+        id: 3,
+        name: 'Tech Day: Gary',
+        description: 'Join us for Tech Day in Gary, a fun-filled event for ages 9-18 featuring interactive workshops...',
+        date: 'August 17, 2024',
+        price: 'Free',
+        photoUri: 'https://s3-alpha-sig.figma.com/img/b64e/9b0c/10f463d3d55716342955c424d473df13?Expires=1721606400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=P2xh4gkh1Owg2c0VeKotpji~XqLxVcGjBUf9nECCLflBb~jzAzeOvyGdEKP3AThjZlKOng7B-57vrAg-HmnBhsQAdC5mNn4A2CasnAvjvSD5vYwI9oItw4bNDxO4Z61ohhOTtO-bKa0JGcZ1601y1VqWoatykPntOoJlJTGniioksnUjMy1GPkBgsIZV-p4oPzfFDSC4x7CaL3Cx19Y~lvpC4Yv2Eqf~0-KGg7LRrDBkRnVYx~TcJapumks2oA3FvMl6odGqVIKXZXrH9Go6g4qlK76b8EeBgho6nalTVPKNWn~Ou3wEROSty2ngPhXI4uxG2aeow54559wxjwh2nQ__',
+        category: 'Robotics',
+        location: 'Faith Community Center, IN',
+		capacity: 'Capacity: 28/32',
+		bookmark: true,
+    },
+];
 
 const BookmarkScreen = () => {
   return (
     <View style={styles.container}>
-      <Text>Bookmark Screen</Text>
+      <Text style={styles.bookmarked}>Bookmarked Programs</Text>
+      <FlatList
+        data={programs.filter(program => program.bookmark)}
+        renderItem={({ item }) => (
+          <ProgramCard program={item} /> // Assuming ProgramCard is a component that displays program details
+        )}
+        keyExtractor={(item) => item.id.toString()}
+        contentContainerStyle={styles.listContainer}
+      />
+	   <Text style={styles.registered}>Registered Programs</Text>
+      <FlatList
+        data={programs.filter(program => program.bookmark)}
+        renderItem={({ item }) => (
+          <ProgramCard program={item} /> // Assuming ProgramCard is a component that displays program details
+        )}
+        keyExtractor={(item) => item.id.toString()}
+        contentContainerStyle={styles.listContainer}
+      />
     </View>
   );
 };
@@ -12,8 +46,25 @@ const BookmarkScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: '#ffffff',
+    padding: 10,
+  },
+  bookmarked:{
+	color:'#2166DE',
+	marginTop: 50,
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  registered:{
+	color:'#A1DD0C',
+	marginTop: 50,
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  listContainer: {
+    paddingBottom: 20,
   },
 });
 
